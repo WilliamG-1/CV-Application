1db888f5dd075188670deffa86f3ee91c1f6afdb
William Gomez <89489831+WilliamG-1@users.noreply.github.com>
2023-03-26 21:27:13 -0700

    App now renders three main form components

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,18 @@ import { Component } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import MainInfo from './components/MainInfo'
+import EducationInfo from './components/EducationInfo'
+import ExperienceInfo from './components/ExperienceInfo'
 
 class App extends Component {
   render () {
     return (
-      <h1>Hello There</h1>
+      <div id="mainApp">
+        <MainInfo />
+        <EducationInfo />
+        <ExperienceInfo />
+      </div>
     )
   }
 }
