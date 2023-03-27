87961bb0699bff515bdcd7f165a2a05b4cefcee4
William Gomez <89489831+WilliamG-1@users.noreply.github.com>
2023-03-27 16:15:40 -0700

    Created states in app component

--- src/App.jsx
+++ src/App.jsx
@@ -8,57 +8,89 @@ import ExperienceInfo from './components/ExperienceInfo'
 
 class App extends Component {
 
-  handleOnTextChange = (e) => {
-    const inputId = e.target.id;
-    const inputText = e.target.value;
+    constructor ( ) {
+      super();
+      this.state = {
+        personalInfo: {
+          firstname: "",
+          lastname: "",
+          email: "",
+          phonenumber: ""
+        },
+        educationEntry: {
+          schoolname: "",
+          title: "",
+          startdate: "",
+          enddate: ""
+        },
+        education: [
 
-    switch (inputId)
-    {
-      case 'firstname':
-        console.log("First name input being changed");
-        break;
-      case 'lastname':
-        console.log("Last name input being changed");
-        break;
-      case 'email':
-        console.log("Email input being changed");
-        break;
+        ],
+        experienceEntry: {
+          companyname: "",
+          title: "",
+          description: "",
+          startdate: "",
+          enddate: ""
+        },
+        experience: [
+          
+        ]
+      }
+      
     }
-  }
 
-  handleOnEmailChange = (e) => {
+    handleOnTextChange = (e) => {
+      const inputId = e.target.id;
+      const inputText = e.target.value;
 
-  }
+      switch (inputId)
+      {
+        case 'firstname':
+          console.log("First name input being changed");
+          break;
+        case 'lastname':
+          console.log("Last name input being changed");
+          break;
+        case 'email':
+          console.log("Email input being changed");
+          break;
+      }
+    }
 
-  handleOnDateChange = (e) => {
+    handleOnEmailChange = (e) => {
 
-  }
+    }
 
-  handleOnAddEducation = () => {
+    handleOnDateChange = (e) => {
 
-  }
+    }
 
-  handleOnAddExperience = () => {
+    handleOnAddEducation = () => {
 
-  }
+    }
+
+    handleOnAddExperience = () => {
+
+    }
 
 
-  render () {
-    return (
-      <div id="main">
-        <div id="information">
-          <MainInfo onChange={this.handleOnTextChange}/>
-          <EducationInfo />
-          <ExperienceInfo />
+    render () {
+      return (
+        <div id="main">
+          <div id="information">
+            <MainInfo onChange={this.handleOnTextChange}/>
+            <EducationInfo />
+            <ExperienceInfo />
+          </div>
+
+          <div id="preview">
+            Hello There
+          </div>
+
         </div>
-
-        <div id="preview">
-          Hello There
-        </div>
-
-      </div>
-    )
-  }
+      )
+    }
 }
 
 export default App
