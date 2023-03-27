aeda47b2dbbbb0f63cb4be4bb81e473d67f63fff
William Gomez <89489831+WilliamG-1@users.noreply.github.com>
2023-03-26 21:25:17 -0700

    Created Education Info Component with forms

---
+++ src/components/EducationInfo.jsx
@@ -0,0 +1,36 @@
+import { Component } from 'react'
+
+export default class EducationInfo extends Component {
+
+    constructor (props) {
+        super(props);
+        this.state = {
+            education : [] ,
+
+            educationInfo : {
+                schooName: "",
+                title: "",
+                beginDate: "yyyy-mm-dd",
+                endDate: "yyyy-mm-dd"
+            }
+        }
+    }
+    handleOnChangeDate = (e) => { 
+        console.log(e.target.value);
+    }
+
+    render () {
+        return (
+            <div id="educationInfo">
+                <h2> Education </h2>
+                <form id="educationInfoForm">
+                    <input type="text" placeholder="School Name"></input>
+                    <input type="text" placeholder="Title"></input>
+                    <input type="date" onChange={this.handleOnChangeDate}></input>
+                    <input type="date" onChange={this.handleOnChangeDate}></input> { /* Change this to a prop lol*/}
+                </form>
+            </div>
+        )
+    }
+}
+
